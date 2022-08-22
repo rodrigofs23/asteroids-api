2216b8fb2852e8ca7dbfc519dd27b56bb528a93a
Rodrigo Souza <rodrigo.souza@nagra.com>
2022-08-22 20:53:52 +0200

    first commit

---
+++ src/main/java/com/asteroids/api/AsteroidsApi.java
@@ -0,0 +1,12 @@
+package com.asteroids.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteroidsApi {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AsteroidsApi.class, args);
+  }
+}
---
+++ src/test/java/com/asteroids/api/AsteroidsApiTests.java
@@ -0,0 +1,11 @@
+package com.asteroids.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteroidsApiTests {
+
+  @Test
+  void contextLoads() {}
+}
